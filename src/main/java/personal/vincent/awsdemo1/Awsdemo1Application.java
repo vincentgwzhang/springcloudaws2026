fd3a7c269496856adf6c96e6264d06491dec3828
vincent <v-zhangv@iata.org>
2026-08-15 15:46:12 +0200

    init develop branch

---
+++ src/main/java/personal/vincent/awsdemo1/Awsdemo1Application.java
@@ -0,0 +1,13 @@
+package personal.vincent.awsdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awsdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awsdemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/personal/vincent/awsdemo1/Awsdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.vincent.awsdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awsdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
